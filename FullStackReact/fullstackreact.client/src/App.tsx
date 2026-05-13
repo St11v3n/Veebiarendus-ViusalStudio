39bdfe0c0325ce402ea34a639be57aff1fefe51d
Opilane TTHK <stiiven111@gmail.com>
2026-05-13 12:39:58 +0300

    some fixes

--- FullStackReact/fullstackreact.client/src/App.tsx
+++ FullStackReact/fullstackreact.client/src/App.tsx
@@ -1,16 +1,16 @@
 import { } from 'react';
 import './App.css';
-import PlanetList from './views/PlanetsList'
-import {Routes, Route, Navigate } from "react-router-dom"
-
+import PlanetsList from './views/PlanetsList';
+import { Routes, Route, Navigate} from "react-router-dom"
 
 
 export default function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Navigate to="/planets" replace />} />
-            <Route path="/planets" element={<PlanetList />} />
-        </Routes>
+
+            <Routes>
+                <Route path="/" element={<Navigate to="/planets" replace />} />
+
+                <Route path="/planets" element={<PlanetsList />} />
+            </Routes>
     );
 }
-
